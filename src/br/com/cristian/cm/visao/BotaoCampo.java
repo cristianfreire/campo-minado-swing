58d136a2931c1b08a8d784d371a782f049a62cf5
cristian.passos <cristian.passos@sp.agence.com.br>
2022-11-24 21:26:20 -0400

    Jogo feito

---
+++ src/br/com/cristian/cm/visao/BotaoCampo.java
@@ -0,0 +1,129 @@
+package br.com.cristian.cm.visao;
+
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
+
+import br.com.cristian.cm.modelo.Campo;
+import br.com.cristian.cm.modelo.CampoEvento;
+import br.com.cristian.cm.modelo.CampoObservador;
+
+@SuppressWarnings("serial")
+public class BotaoCampo extends JButton implements CampoObservador, MouseListener{
+	
+	private final Color BG_PADRAO = new Color(184, 184, 184);
+	private final Color BG_MARCADO = new Color(8, 179, 247);
+	private final Color BG_EXPLODIDO = new Color(189, 66, 68);
+	private final Color TEXTO_VERDE = new Color(0, 100, 0);
+	
+	private Campo campo;
+	
+	public BotaoCampo(Campo campo) {
+		this.campo = campo;
+		setBackground(BG_PADRAO);
+		setOpaque(true);
+		setBorder(BorderFactory.createBevelBorder(0));
+		
+		addMouseListener(this);
+		campo.registrarObservador(this);
+	}
+	
+	@Override
+	public void eventoOcorreu(Campo campo, CampoEvento evento) {
+		switch(evento) {
+		case ABRIR:
+			aplicarEstiloAbrir();
+			break;
+		case MARCAR:
+			aplicarEstiloMarcar();
+			break;
+		case EXPLODIR:
+			aplicarEstiloExplodir();
+			break;
+		default:
+			aplicarEstiloPadrao();
+			setBorder(BorderFactory.createBevelBorder(0));
+			
+		}
+		
+		SwingUtilities.invokeLater(() -> {
+			repaint();
+			validate();
+		});
+	}
+
+	private void aplicarEstiloPadrao() {
+		setBackground(BG_PADRAO);
+		setText("");
+	}
+
+	private void aplicarEstiloExplodir() {
+		setBackground(BG_EXPLODIDO);
+		setForeground(Color.white);
+		setText("X");
+	}
+
+	private void aplicarEstiloMarcar() {
+		setBackground(BG_MARCADO);
+		setForeground(Color.black);
+		setText("P");
+	}
+
+	private void aplicarEstiloAbrir() {
+		if(campo.isMinado()) {
+			setBackground(BG_EXPLODIDO);
+			return;
+		}
+		
+		setBackground(BG_PADRAO);
+		setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		switch (campo.minasnNaVizinhanca()) {
+		case 1: 
+			setForeground(TEXTO_VERDE);
+			break;
+		case 2:
+			setForeground(Color.BLUE);
+			break;
+		case 3:
+			setForeground(Color.YELLOW);
+			break;
+		case 4:
+		case 5:
+		case 6:
+			setForeground(Color.RED);
+			break;
+		default:
+			setForeground(Color.PINK);
+		}
+		
+		String valor = !campo.vizinhancaSegura() ? campo.minasnNaVizinhanca() +  "" : "";
+		setText(valor);
+	}
+
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if(e.getButton() == 1) {
+			System.out.println("botao esquerdo");
+			campo.abrir();
+		}else {
+			campo.alternarMarcacao();
+			System.out.println("botao dieito");
+		}
+	}
+	
+	
+	public void mouseClicked(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+
+
+	
+
+}
